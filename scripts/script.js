c4236eb7f7839f39a8b822177cf8c030e98fa778
ajaphid <aamonett@andrew.cmu.edu>
2024-11-21 11:31:26 -0500

    page min height set responsive to navbar and footer height

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,36 @@ import { loadProjects } from './projects.js';
 
 addEventListener("DOMContentLoaded", () => {
     console.log('HTML loaded');
+    setMinPageHeight();
 
     loadProjects();
 
+    window.addEventListener('resize', setMinPageHeight);
+
 });
+
+function setMinPageHeight() {
+    // pages should at least fill the window vertically
+    // except the last page, which should also account for the footer
+
+    const navbar = document.querySelector('nav');
+    const pages = document.querySelectorAll('.page');
+    const navbarHeight = navbar.offsetHeight;
+    const footerHeight = document.querySelector('footer').offsetHeight;
+    if (pages.length > 0) {
+        const lastPage = pages[pages.length - 1];
+
+        pages.forEach(page => {
+            if (lastPage === page) {
+                page.style.backgroundColor = 'blue';
+                page.style.minHeight = `${window.innerHeight - navbarHeight - footerHeight}px`;
+            } else { 
+                page.style.backgroundColor = 'red';
+                page.style.minHeight = `${window.innerHeight - navbarHeight}px`;
+            }
+        });
+    }
+}
 
 
 // smooth scroll to sections on nav
