1314b173b3b225f60cfd7b9fcd7fa7850ff421cd
ajaphid <aamonett@andrew.cmu.edu>
2024-11-21 17:48:02 -0500

    interactive navbar features

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,35 @@
 import { loadProjects } from './projects.js';
 
+let pages = document.querySelectorAll('.page');
+let navLinks = document.querySelectorAll('.nav-link a');
+
+// the css smooth scroll behavior is already done thru html and css
+// I just need to add the 'selected' class to the clicked link
+// and remove it from the current one (if it isnt the same)
+navLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+        const prevSelectedLink = document.querySelector('.selected-nav a');
+        const selectedLink = e.target;
+        console.log(prevSelectedLink);
+        console.log(selectedLink);
+        if (prevSelectedLink !== selectedLink) {
+            prevSelectedLink.parentElement.classList.remove('selected-nav');
+            selectedLink.parentElement.classList.add('selected-nav');
+        }
+    })
+})
+
+// the same nav link highlighting should be done when scrolling to a section
+window.addEventListener('scroll', () => {
+    pages.forEach(page => {
+        let top = window.scrollY;
+        let offset = page.offsetTop;
+        let height = page.offsetHeight;
+        let id = page.getAttribute('id');
+    })
+});
+
+
 addEventListener("DOMContentLoaded", () => {
     console.log('HTML loaded');
     setMinPageHeight();
@@ -37,6 +67,9 @@ function setMinPageHeight() {
     html.style.scrollPadding = `${navbarHeight}px 0 0 0`;
 }
 
-// highlight current section on navbar on scroll
+// bold on hover for navbar links without altering the layout
+// highlight current section on navbar on "scroll to" or click selection
 
 // other interactions like hover effects on buttons, etc.
+
+// rotating and color changing star svg on hover for the home button
