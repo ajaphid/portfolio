678cc472dc691c622987e8960c24afd145c0260e
ajaphid <aamonett@andrew.cmu.edu>
2024-11-21 19:34:41 -0500

    navbar smooth scrolling auto selects links, added fix for selecting on clicked nav scroll

--- scripts/script.js
+++ scripts/script.js
@@ -2,41 +2,60 @@ import { loadProjects } from './projects.js';
 
 let pages = document.querySelectorAll('.page');
 let navLinks = document.querySelectorAll('.nav-link a');
-
-// the css smooth scroll behavior is already done thru html and css
-// I just need to add the 'selected' class to the clicked link
-// and remove it from the current one (if it isnt the same)
-navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-        const prevSelectedLink = document.querySelector('.selected-nav a');
-        const selectedLink = e.target;
-        console.log(prevSelectedLink);
-        console.log(selectedLink);
-        if (prevSelectedLink !== selectedLink) {
-            prevSelectedLink.parentElement.classList.remove('selected-nav');
-            selectedLink.parentElement.classList.add('selected-nav');
-        }
-    })
-})
-
-// the same nav link highlighting should be done when scrolling to a section
-window.addEventListener('scroll', () => {
-    pages.forEach(page => {
-        let top = window.scrollY;
-        let offset = page.offsetTop;
-        let height = page.offsetHeight;
-        let id = page.getAttribute('id');
-    })
-});
-
+let logo = document.querySelector('.logo');
 
 addEventListener("DOMContentLoaded", () => {
     console.log('HTML loaded');
     setMinPageHeight();
-
     loadProjects();
 
     window.addEventListener('resize', setMinPageHeight);
+
+    // the first child of the nav links should be selected by default
+    navLinks[0].parentElement.classList.add('selected-nav');
+    // flag to prevent selecting a nav link while scrolling from click
+    let isMidScroll = false;
+
+    // the css smooth scroll behavior is already done thru html and css
+    // I just need to add the 'selected' class to the clicked link
+    // and remove it from the current one (if it isnt the same)
+    navLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            isMidScroll = true;
+            handleNavSelect(e.target);
+            setTimeout(() => { isMidScroll = false; }, 1000);
+        })
+    })
+
+    // this is also done for the first link when clicking the logo
+    logo.addEventListener('click', (e) => {
+        isMidScroll = true;
+        handleNavSelect(navLinks[0]);
+        setTimeout(() => { isMidScroll = false; }, 1000);
+    })
+
+    // the corresponding nav link should be selected when scrolling to a page
+    // event for scroll
+    // window.scrollY gives you how much you have scrolled
+    // how far is the top of the page from the top of the window (minus the navbar height)
+    // change pages when the distance scrolled is more than the distance the page is offset from the top
+    // added a 30% buffer to switch the nav link before the page is fully in view
+    window.addEventListener('scroll', () => {
+        // don't select a nav link on scroll if the user is scrolling from a nav link click
+        if (isMidScroll) return;
+        const navbarHeight = document.querySelector('.navbar').offsetHeight;
+
+        pages.forEach(page => {
+            let pageHeight = page.offsetHeight;
+            let pageBuffer = Math.min(pageHeight, window.innerHeight - navbarHeight) * 0.3
+            let pageOffset = page.offsetTop - navbarHeight - pageBuffer;
+            let scrollY = window.scrollY;
+
+            if (scrollY >= pageOffset && scrollY < (pageOffset + pageHeight)) {
+                handleNavSelect(document.querySelector(`.nav-link a[href="#${page.id}"]`));
+            }
+        });
+    });
 
 });
 
@@ -67,9 +86,12 @@ function setMinPageHeight() {
     html.style.scrollPadding = `${navbarHeight}px 0 0 0`;
 }
 
-// bold on hover for navbar links without altering the layout
-// highlight current section on navbar on "scroll to" or click selection
-
-// other interactions like hover effects on buttons, etc.
+function handleNavSelect(linkTo) {
+    const prevSelectedLink = document.querySelector('.selected-nav a');
+    if (prevSelectedLink !== linkTo) {
+        prevSelectedLink.parentElement.classList.remove('selected-nav');
+        linkTo.parentElement.classList.add('selected-nav');
+    }
+}
 
 // rotating and color changing star svg on hover for the home button
